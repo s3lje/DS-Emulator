1960842d8f9cac05a35d15cc4d1fade3fc3fc0dd
s3lje <emil.seljestokken@outlook.com>
2026-03-22 16:26:44 +0100

    Added the 512 byte header for .nds roms

---
+++ src/memory/rom.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <cstdint>
+#include <vector>
+#include <string>
+
+struct NDSHeader {
+    char     gameTitle[12];
+    char     gameCode[4];
+    char     makerCode[2];
+    uint8_t  unitCode;
+    uint8_t  encryptionSeed;
+    uint8_t  deviceCapacity;
+    uint8_t  reserved[9];
+    uint8_t  region;
+    uint8_t  romVersion;
+    uint8_t  autostart;
+
+    uint32_t arm9RomOffset;
+    uint32_t arm9EntryAddress;
+    uint32_t arm9RamAddress;
+    uint32_t arm9Size;
+
+    uint32_t arm7RomOffset;
+    uint32_t arm7EntryAddress;
+    uint32_t arm7RamAddress;
+    uint32_t arm7Size;
+} __attribute__((packed));
