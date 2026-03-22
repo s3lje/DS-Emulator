3fca57a8905b37114f190e33be2562b542874adf
s3lje <emil.seljestokken@outlook.com>
2026-03-22 16:43:00 +0100

    Added logic for loading roms and added the rom structure to the nds class

--- src/nds.cpp
+++ src/nds.cpp
@@ -1,8 +1,24 @@
 #include "nds.h"
 #include <iostream>
+#include <fstream>
+#include <cstring>
+
 
 bool NDS::loadROM(const std::string& path){
-    std::cout << "Loading ROM: " << path << std::endl;
+    std::ifstream f(path, std::ios::binary);
+    if (!f) return false;
+
+    f.seekg(0, std::ios::end);
+    rom.resize(f.tellg());
+    f.seekg(0);
+    f.read(reinterpret_cast<char*>(rom.data()), rom.size());
+
+    header = *reinterpret_cast<NDSHeader*>(rom.data());
+
+    std::cout << "Game: " << header.gameTitle << std::endl;
+    std::cout << "ARM9: " << header.arm9Size << "bytes @ 0x"
+              << std::hex << header.arm9RamAddress << std::endl;
+
     return true;
 }
 
--- src/nds.h
+++ src/nds.h
@@ -1,8 +1,16 @@
 #pragma once
 #include <string>
+#include <vector>
+#include <cstdint>
+#include "memory/rom.h"
 
 class NDS {
     public:
         bool loadROM(const std::string& path);
         void run();
+
+    private:
+        NDSHeader header {};
+        std::vector<uint8_t> rom;
+        std::array<uint8_t, 0x400000> mainRAM {};
 };
