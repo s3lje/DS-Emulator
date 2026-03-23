7769e805ae9f6e070e352b61ec7117a0d14ad8af
s3lje <emil.seljestokken@outlook.com>
2026-03-23 09:32:42 +0100

    Added function for checking conditionals

--- src/arm/arm.cpp
+++ src/arm/arm.cpp
@@ -35,4 +35,25 @@ void ARM::step(){
     }
 }
 
+bool ARM::checkCond(uint32_t cond){
+    switch (cond) {
+        case 0x0: return  flagZ();                          // EQ = equal
+        case 0x1: return !flagZ();                          // NE = not squal
+        case 0x2: return  flagC();                          // CS = carry set
+        case 0x3: return !flagC();                          // CC = carry clear
+        case 0x4: return  flagN();                          // MI = minus
+        case 0x5: return !flagN();                          // PL = plus
+        case 0x6: return  flagV();                          // VS = overflow set
+        case 0x7: return !flagV();                          // VC = overflow clear
+        case 0x8: return  flagC() && !flagZ();              // HI = unsigned higher
+        case 0x9: return !flagC() ||  flagZ();              // LS = unsigned lower or same
+        case 0xA: return  flagN() ==  flagV();              // GE = signed >=
+        case 0xB: return  flagN() !=  flagV();              // LT = signed 
+        case 0xC: return !flagZ() && (flagN() == flagV());  // GT = signed >
+        case 0xD: return  flagZ() || (flagN() != flagV());  // LE = signed <=
+        case 0xE: return true;                              // AL = always
+        default:  return false;                             // NV = never
+    }
+}
+
 
