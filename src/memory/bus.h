abc23bed316532e93963d817d557351cd64afa72
s3lje <emil.seljestokken@outlook.com>
2026-04-13 08:39:31 +0200

    Added graphical VRAMS and palettes to the bus class

--- src/memory/bus.h
+++ src/memory/bus.h
@@ -14,9 +14,15 @@ class Bus {
         void write16(uint32_t addr, uint16_t val);
         void write32(uint32_t addr, uint32_t val);
 
-        std::array<uint8_t, 0x400000> mainRAM   {};  // 4 MB @ 0x02000000
-        std::array<uint8_t, 0x8000>   sharedWRAM{};  // 32 KB @ 0x03000000
-        std::vector<uint8_t>          rom;           // cartridge ROM
+        std::array<uint8_t, 0x400000> mainRAM   {}; // 4 MB @ 0x02000000
+        std::array<uint8_t, 0x8000>   sharedWRAM{}; // 32 KB @ 0x03000000
+        std::vector<uint8_t>          rom;          // cartridge ROM
+    
+        std::array<uint8_t, 0x100000> vramA  {};     // 128KB, BG tiles engine A
+        std::array<uint8_t, 0x100000> vramB  {};     // 128KB, BG tiles engine B
+        std::array<uint8_t, 0x20000>  vramE  {};     // 64KB,  palette/BG engine B
+        std::array<uint8_t, 0x4000>   oam    {};     // 4KB,   sprite attributes
+        std::array<uint8_t, 0x800>    pallete{};    // 2KB,   color palettes
     
         InterruptController irq9;
         InterruptController irq7; 
